67080372f5a03efa1f1bdb06323996ac54ea84b6
Mohamed Safras <mohamed.safras@acentura.com>
2024-08-03 23:00:19 +0530

    drag & drop component has been added

---
+++ src/app/dashboard/upload/page.tsx
@@ -0,0 +1,8 @@
+import UploadPage from "@/src/page/upload/UploadPage";
+import React from "react";
+
+const page = () => {
+  return <UploadPage />;
+};
+
+export default page;
---
+++ src/page/upload/UploadPage.tsx
@@ -0,0 +1,12 @@
+import FileUploader from "@/src/components/custom/FileUploader";
+import React from "react";
+
+const UploadPage = () => {
+  return (
+    <div>
+      <FileUploader />
+    </div>
+  );
+};
+
+export default UploadPage;
